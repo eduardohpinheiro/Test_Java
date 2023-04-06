a856c77d533f68d56f13483ccf0f610b9653faa5
Eduardo Pinheiro <111510684+eduardohpinheiro@users.noreply.github.com>
2023-04-06 01:17:18 -0300

    Update Exercicio1.java

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,19 +1,17 @@
 # Test_Java
 public class Fatura {
-    private String numero;
-    private String descricao;
-    private int quantidade;
-    private double preco;
-
-    // Construtor
+    public String numero;
+    public  String descricao;
+    public  int quantidade;
+    public  double preco;
+    
     public Fatura(String numero, String descricao, int quantidade, double preco) {
         this.numero = numero;
         this.descricao = descricao;
         setQuantidade(quantidade);
         setPreco(preco);
     }
-
-    // Métodos get e set para número
+    
     public String getNumero() {
         return numero;
     }
@@ -21,21 +19,15 @@ public class Fatura {
     public void setNumero(String numero) {
         this.numero = numero;
     }
-
-    // Métodos get e set para descrição
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
-    // Métodos get e set para quantidade
     public int getQuantidade() {
         return quantidade;
     }
-
     public void setQuantidade(int quantidade) {
         if (quantidade >= 0) {
             this.quantidade = quantidade;
@@ -43,12 +35,9 @@ public class Fatura {
             this.quantidade = 0;
         }
     }
-
-    // Métodos get e set para preço
     public double getPreco() {
         return preco;
     }
-
     public void setPreco(double preco) {
         if (preco >= 0.0) {
             this.preco = preco;
@@ -56,9 +45,28 @@ public class Fatura {
             this.preco = 0.0;
         }
     }
-
-    // Método para calcular o valor total da fatura
     public double getTotalFatura() {
         return quantidade * preco;
+    }
+}
+
+// app de teste da classe fatura 
+public class FaturaTeste {
+    public static void main(String[] args) {
+      
+        Fatura fatura = new Fatura("123", "Mouse", 5, 12.99);
+
+        System.out.println("Número: " + fatura.getNumero());
+        System.out.println("Descrição: " + fatura.getDescricao());
+        System.out.println("Quantidade: " + fatura.getQuantidade());
+        System.out.println("Preço: " + fatura.getPreco());
+        System.out.println("Total da Fatura: " + fatura.getTotalFatura());
+     
+        fatura.setQuantidade(10);
+        fatura.setPreco(9.99);
+      
+        System.out.println("Quantidade atualizada: " + fatura.getQuantidade());
+        System.out.println("Preço atualizado: " + fatura.getPreco());
+        System.out.println("Total da Fatura atualizado: " + fatura.getTotalFatura());
     }
 }
